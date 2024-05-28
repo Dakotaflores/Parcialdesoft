91568e7783dba75aef6bdb7a56ce55378c399065
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-05-28 16:26:26 -0400

    Add files via upload

---
+++ Mathematics.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Runtime.CompilerServices;
+using System.Xml.XPath;
+
+namespace TestDemo; 
+public static class Mathematics { 
+//Calculo de fuerza
+public static double Calculodefuerza(double masa, double aceleracion) { 
+return (masa * aceleracion); 
+} 
+
+//Calculo de trabajo
+public static double Calculodetrabajo(double fuerza, double distancia) { 
+return (fuerza * distancia); 
+} 
+
+//Calculo de energia cinetica 
+public static double EnergiaCinetica(double masa) { 
+double velocidad = 4 * 4; 
+double result = (velocidad * masa * 0.5);
+return (result); 
+} 
+} 
+
+
